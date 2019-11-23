e5fab5a0d9c68930bbc0d9b528301a2c36769515
Johan R <jreinhed@protonmail.com>
2019-11-23 02:22:23 +0100

    Initial commit

---
+++ read_config.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "read_config.h"
+
+/*
+ * Find an option in the opts array.
+ * Arguments:
+ * 	opt - Option name to look for
+ * 	opts - Array of options
+ * Returns place in array if found, 0 otherwise.
+ */
+int find_opt(char *opt, struct config_opt *opts)
+{
+	int n = 0;
+	while (opts[n].opt_name != NULL) {
+		if (strcmp(opt, opts[n].opt_name) == 0)
+			return n;
+		else
+			n++;
+	}
+
+	return -1;
+}
+
+/*
+ * Strip leading and trailing whitespace from str.
+ * Modifies str!
+ */
+void strip_whitespace(char **str)
+{
+	while (isspace(**str)) {
+		(*str)++;
+	}
+
+	char *end = strchr(*str, '\0') - 1;
+
+	while (isspace(*end)) {
+		end--;
+	}
+	*(end+1) = '\0';
+}
+
+/*
+ * Parse config file.
+ * Arguments:
+ * 	filename - name of config file
+ * 	opts - pointer to array of options
+ * Returns zero if successful, negative number otherwise.
+ */
+int read_config(char *filename, struct config_opt *opts)
+{
+	char line[2048];
+	char *opt, *val;
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+		return -1;
+
+	while (fgets(line, 2048, file) != NULL) {
+		/* TODO: Allow in-line comments */
+		if (line[0] == '#' || line[0] == '\n')
+			continue;
+
+		if ((opt = strtok(line, "=")) != NULL) {
+			strip_whitespace(&opt);
+			int n = find_opt(opt, opts);
+
+			/* Ignore unknown options */
+			if (n < 0) {
+				continue;
+			}
+			
+			val = strtok(NULL, "=");
+			strip_whitespace(&val);
+
+			int datatype = opts[n].datatype;
+
+			if (datatype == DATATYPE_STRING) {
+				char **p = opts[n].value;
+				*p = val;
+			} else if (datatype == DATATYPE_INT) {
+				int *p = opts[n].value;
+				*p = strtol(val, NULL, 0);
+			}
+		}
+	}
+	fclose(file);
+	return 0;
+}
---
+++ read_config.h
@@ -0,0 +1,15 @@
+#ifndef READ_CONFIG_H
+#define READ_CONFIG_H
+
+#define DATATYPE_STRING 1
+#define DATATYPE_INT 2
+
+struct config_opt {
+	char *opt_name;
+	void *value;
+	int datatype;
+};
+
+int read_config(char *, struct config_opt *);
+
+#endif /* READ_CONFIG_H */
